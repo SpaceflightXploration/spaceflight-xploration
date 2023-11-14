0689db8c7ade09e13b9c3465e9073666d3e98228
SpaceflightXploration <150655879+SpaceflightXploration@users.noreply.github.com>
2023-11-14 12:14:55 +0800

    Add files via upload

---
+++ user/js/auth.js
@@ -0,0 +1,90 @@
+// Wrap your code within the 'DOMContentLoaded' event listener to ensure it executes after the page content is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    const auth = firebase.auth();
+    window.addEventListener("message", receiveMessage, false);
+
+    function receiveMessage(event) {
+        if (event.data) {
+            var currentUserUID = event.data;
+            // Do something with the received UID from the app
+            console.log("Received UID from the app: " + currentUserUID);
+            // You can now use currentUserUID in your website logic
+            // Fetch user-specific data
+    //const currentUserUID = 'zmI32D9nKiYVByjLkq3ULsh9s6p2';
+    // Assuming this UID is hardcoded for testing
+    const userRef = firebase.database().ref('userdata/' + currentUserUID);
+
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in
+            const uid = user.uid;
+            console.log('User is signed in');
+            // Redirect or perform actions for a signed-in user
+        } else {
+            // User is signed out
+            console.log('No user signed in');
+            // Redirect or perform actions for a signed-out user
+        }
+    });
+
+    userRef.once('value')
+        .then((snapshot) => {
+            const userData = snapshot.val();
+            console.log('Snapshot:', snapshot.val());
+
+            if (userData !== null) {
+                // Data exists; populate input fields
+                document.querySelector('input[placeholder="Username"]').value = userData.username || '';
+                document.querySelector('input[placeholder="Bio"]').value = userData.bio || '';
+                document.querySelector('input[placeholder="Email"]').value = userData.email || '';
+                document.querySelector('input[placeholder="Phone Number"]').value = userData.phoneNumber || '';
+                document.querySelector('#pfp').src = userData.profile_url || '';
+            } else {
+                console.error('User data does not exist or is null.');
+                // Handle the case where the data is not available
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching user data:', error);
+        });
+        
+        }
+    }
+
+    // Add an event listener to the 'Save Changes' button
+    const saveChangesButton = document.querySelector('.save-changes-btn');
+    saveChangesButton.addEventListener('click', saveChanges);
+
+    
+    function saveChanges(event) {
+        event.preventDefault();
+
+        const ref = firebase.database().ref('userdata/' + currentUserUID);
+
+        const updatedUsername = document.querySelector('input[placeholder="Username"]').value;
+        const updatedBio = document.querySelector('input[placeholder="Bio"]').value;
+        const updatedEmail = document.querySelector('input[placeholder="Email"]').value;
+        const updatedPhoneNumber = document.querySelector('input[placeholder="Phone Number"]').value;
+
+        if (updatedUsername) {
+            // Update the user data
+            ref.update({
+                username: updatedUsername,
+                bio: updatedBio,
+                email: updatedEmail, // Fixed property name
+                phoneNumber: updatedPhoneNumber
+            }).then(() => {
+                console.log('User data updated successfully!');
+                // Handle success - display a message, navigate to another page, etc.
+            }).catch((error) => {
+                console.error('Error updating user data:', error);
+                // Handle the error
+            });
+        } else {
+            // Prompt the user to enter a valid username
+            if (!updatedUsername) {
+                alert("Please provide a valid username.");
+            }
+        }
+    }
+});
---
+++ user/js/changeProfile.js
@@ -0,0 +1,105 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const auth = firebase.auth();
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+        const storageRef = firebase.storage().ref();
+             imageRef = storageRef.child('userdata/profile/' + user.uid +'.jpeg'); // Define the path to upload
+            // User is signed in
+            console.log('Awaiting Profile Picture Update...');
+            // Redirect or perform actions for a signed-in user
+        } else {
+            // User is signed out
+            console.log('System Error: No User Signed In (but can change profile');
+                alert('System Error: No User Signed In. Please Go Back.');
+            // Redirect or perform actions for a signed-out user
+        }
+    });
+    
+    const changeProfileButton = document.querySelector('#change-profile-btn');
+
+    changeProfileButton.addEventListener('click', selectImage);
+});
+
+function selectImage() {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = 'image/*';
+        fileInput.style.display = 'none'; // Hide the input element
+        fileInput.addEventListener('change', handleFileSelection);
+        document.body.appendChild(fileInput); // Append the input element to the body
+        fileInput.click();
+    }
+
+    function handleFileSelection(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+            const img = new Image();
+            img.onload = function() {
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+
+                const minSize = Math.min(img.width, img.height);
+                canvas.width = minSize;
+                canvas.height = minSize;
+
+                ctx.drawImage(img, 0, 0, minSize, minSize, 0, 0, minSize, minSize);
+
+                const croppedDataURL = canvas.toDataURL('image/jpeg');
+                // Use croppedDataURL to update Firebase storage or perform other actions
+                const pfp = document.getElementById('pfp');
+                pfp.src = croppedDataURL;
+                uploadToFirebaseStorage(croppedDataURL); // Display the cropped image on the page
+            };
+
+            img.src = event.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    }
+
+
+
+function uploadToFirebaseStorage(croppedDataURL) {
+    // Convert the base64 string to a Blob
+    const byteCharacters = atob(croppedDataURL.split(',')[1]);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const imageBlob = new Blob([byteArray], { type: 'image/jpeg' });
+
+    // Upload the Blob to Firebase Storage
+    imageRef.put(imageBlob).then((snapshot) => {
+        console.log('Uploaded the image!', snapshot);
+        alert('Image Uploaded.');
+
+        // Get the download URL of the uploaded image
+        snapshot.ref.getDownloadURL().then((downloadURL) => {
+            // Additional handling or updating user profile with the uploaded image link
+            console.log('Download URL:', downloadURL);
+
+            // Update the user profile with the image URL
+            const user = firebase.auth().currentUser;
+            if (user) {
+                const userId = user.uid;
+                const userDataRef = firebase.database().ref('userdata/' + userId);
+                userDataRef.update({ profile_url: downloadURL }).then(() => {
+                    console.log('Profile URL saved in the database');
+                    alert('URL saved. Please wait for a few moments before it changes in every applications.');
+                }).catch((error) => {
+                    console.error('Error saving profile URL:', error);
+                    alert('Error saving in the database.');
+                });
+            }
+        }).catch((error) => {
+            console.error('Error getting download URL:', error);
+            alert('ERROR getting Profile URL. Please resubmit your profile.');
+        });
+    }).catch((error) => {
+        console.error('Error uploading the image:', error);
+        alert('Error uploading the image.');
+    });
+}
